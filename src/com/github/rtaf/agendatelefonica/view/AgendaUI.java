223ae605dcb1772576c038821d844154f1c5a29a
Radu Tafuni <rtafuni20@yahoo.com>
2016-09-29 13:27:47 +0300

    adding a JTable for abonati and a TableModel

--- src/com/github/rtaf/agendatelefonica/view/AgendaUI.java
+++ src/com/github/rtaf/agendatelefonica/view/AgendaUI.java
@@ -5,19 +5,53 @@
  */
 package com.github.rtaf.agendatelefonica.view;
 
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author rtafuni
  */
 public class AgendaUI extends javax.swing.JFrame {
 
+    DefaultTableModel modelTabelAbonati;
+    
     /**
      * Creates new form AgendaUI
      */
     public AgendaUI() {
         initComponents();
+        DisableTextFieldsUI();
+        adaugareInTabelAbonati();
     }
 
+    /**
+     * enables text fields from UI
+     */
+    void EnableTextFieldsUI() {
+        textNume.setEnabled(true);
+        textPrenume.setEnabled(true);
+        textCNP.setEnabled(true);
+        textNrTel.setEnabled(true);
+        textNume.requestFocus();
+    }
+
+    /**
+     * disables text fields from UI
+     */
+    void DisableTextFieldsUI() {
+        textNume.setEnabled(false);
+        textPrenume.setEnabled(false);
+        textCNP.setEnabled(false);
+        textNrTel.setEnabled(false);
+    }
+
+    void adaugareInTabelAbonati() {
+        String[] numeColoane = {"ID", "Nume", "Prenume", "CNP", "NrTel"};
+        modelTabelAbonati = new DefaultTableModel(null, numeColoane);
+        jTableAbonati.setModel(modelTabelAbonati);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -28,17 +62,17 @@ public class AgendaUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
+        jLabelNume = new javax.swing.JLabel();
+        jLabelPrenume = new javax.swing.JLabel();
+        jLabelCNP = new javax.swing.JLabel();
+        textNume = new javax.swing.JTextField();
+        textPrenume = new javax.swing.JTextField();
+        jLabelNrTel = new javax.swing.JLabel();
+        textCNP = new javax.swing.JTextField();
+        textNrTel = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        butonAbonatNou = new javax.swing.JButton();
+        jTableAbonati = new javax.swing.JTable();
+        butonAdauga = new javax.swing.JButton();
         butonSalavare = new javax.swing.JButton();
         butonStergere = new javax.swing.JButton();
         butonActualizare = new javax.swing.JButton();
@@ -48,13 +82,13 @@ public class AgendaUI extends javax.swing.JFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Date Abonat:", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 0, 12), java.awt.Color.blue)); // NOI18N
 
-        jLabel1.setText("Nume");
+        jLabelNume.setText("Nume");
 
-        jLabel2.setText("Prenume");
+        jLabelPrenume.setText("Prenume");
 
-        jLabel3.setText("Telefon");
+        jLabelCNP.setText("CNP");
 
-        jLabel4.setText("eMail");
+        jLabelNrTel.setText("NrTel");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -63,44 +97,41 @@ public class AgendaUI extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabelPrenume, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabelNume, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabelCNP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabelNrTel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField1)
-                    .addComponent(jTextField2)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
-                    .addComponent(jTextField3))
+                    .addComponent(textNume)
+                    .addComponent(textPrenume)
+                    .addComponent(textNrTel, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+                    .addComponent(textCNP))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(textNume)
+                    .addComponent(jLabelNume, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabelPrenume, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textPrenume, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(textCNP)
+                    .addComponent(jLabelCNP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(textNrTel)
+                    .addComponent(jLabelNrTel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTableAbonati.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -111,9 +142,14 @@ public class AgendaUI extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTableAbonati);
 
-        butonAbonatNou.setText("Abonat Nou");
+        butonAdauga.setText("Adauga");
+        butonAdauga.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                butonAdaugaActionPerformed(evt);
+            }
+        });
 
         butonSalavare.setText("Salvare");
 
@@ -129,7 +165,7 @@ public class AgendaUI extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(butonAbonatNou, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
+                .addComponent(butonAdauga, javax.swing.GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(butonSalavare, javax.swing.GroupLayout.DEFAULT_SIZE, 71, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -156,7 +192,7 @@ public class AgendaUI extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(butonAbonatNou)
+                    .addComponent(butonAdauga)
                     .addComponent(butonSalavare)
                     .addComponent(butonStergere)
                     .addComponent(butonActualizare)
@@ -166,6 +202,11 @@ public class AgendaUI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void butonAdaugaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_butonAdaugaActionPerformed
+        // TODO add your handling code here:
+        EnableTextFieldsUI();
+    }//GEN-LAST:event_butonAdaugaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -203,21 +244,21 @@ public class AgendaUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton butonAbonatNou;
     private javax.swing.JButton butonActualizare;
+    private javax.swing.JButton butonAdauga;
     private javax.swing.JButton butonAnulare;
     private javax.swing.JButton butonSalavare;
     private javax.swing.JButton butonStergere;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabelCNP;
+    private javax.swing.JLabel jLabelNrTel;
+    private javax.swing.JLabel jLabelNume;
+    private javax.swing.JLabel jLabelPrenume;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTable jTableAbonati;
+    private javax.swing.JTextField textCNP;
+    private javax.swing.JTextField textNrTel;
+    private javax.swing.JTextField textNume;
+    private javax.swing.JTextField textPrenume;
     // End of variables declaration//GEN-END:variables
 }
